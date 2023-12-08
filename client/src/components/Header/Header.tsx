fd9479bdf0b2d3d07c125540cfb0cc18140a086a
Bazl1 <dazman3243243@gmail.com>
2023-12-08 17:48:11 +0200

    Add user img

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -19,11 +19,11 @@ const Header = () => {
                         store.isAuth ?
                             <div className={s.header__user_box}>
                                 <div className={s.header__user_avatar}>
-                                    {/* {store.user.imageUri == '' ?
-                                        <img className={s.header__user_img} src={user} alt="user" />
-                                        : <img className={s.header__user_img} src={store.user.imageUri} alt="user" />} */}
+                                    {typeof store.user.imageUri === 'string' && store.user.imageUri !== '' ?
+                                        <img className={s.header__user_img} src={store.user.imageUri} alt="user" />
+                                        : <img className={s.header__user_img} src={user} alt="user" />}
                                 </div>
-                                {/* <div className={s.header__user_name}>Welcome ~ {store.user.name}</div> */}
+                                <div className={s.header__user_name}>Welcome ~ {store.user.name}</div>
                             </div>
                             :
                             <Link to={'/login'} className={s.header__auth_btn}>Login</Link>
