d622b4f846cd671f678d784aa7c9e215ac4baf9f
Bazl1 <dazman3243243@gmail.com>
2023-12-14 15:47:27 +0200

    Add PostService

--- client/src/service/PostsService.ts
+++ client/src/service/PostsService.ts
@@ -2,7 +2,7 @@ import $api from "../assets/utils/axios/index";
 import { AxiosResponse } from "axios";
 import { CategoryResponse } from "../models/response/CategoryResponse"
 
-export default class SettingService {
+export default class PostService {
     static async fetchCategory(): Promise<AxiosResponse<CategoryResponse[]>> {
         return $api.get<CategoryResponse[]>('/topics')
     }
