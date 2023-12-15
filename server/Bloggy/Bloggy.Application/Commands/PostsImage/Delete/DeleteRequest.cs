683cc2aaf988fa0373688a54bf3a97f9f2d22b1d
ReZerVV <kirillmorozov547@gmail.com>
2023-12-15 16:37:47 +0200

    Update post image controller

--- server/Bloggy/Bloggy.Application/Commands/PostsImage/Delete/DeleteRequest.cs
+++ server/Bloggy/Bloggy.Application/Commands/PostsImage/Delete/DeleteRequest.cs
@@ -1,9 +1,7 @@
 using MediatR;
-using Microsoft.AspNetCore.Http;
 
 namespace Bloggy.Application.Commands.PostsImage.Delete;
 
 public record DeleteRequest(
-    Guid Id,
-    IFormFile Image
+    Guid Id
 ) : IRequest<DeleteResponse>;
